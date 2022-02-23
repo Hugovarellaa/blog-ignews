2a194ef3b2e12f78d20d8669787014ae308038d5
hugovarellaa <hugovarellaa@gmail.com>
2022-02-23 11:32:54 -0300

    Configurando os webhooks

---
+++ blog-ignews/src/pages/api/webhooks.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+};
