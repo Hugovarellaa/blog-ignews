954e9826fa0454452ecdec2f27c9661e2df81c16
hugovarellaa <hugovarellaa@gmail.com>
2022-02-23 11:43:15 -0300

    Recebendo e tratando o evento do stripe

--- blog-ignews/src/pages/api/webhooks.ts
+++ blog-ignews/src/pages/api/webhooks.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -16,9 +18,26 @@ export const config = {
   },
 };
 
+const eventRelevant = new Set(["checkout.session.completed"]);
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const buf = await buffer(req);
+    const webhook = process.env.STRIPE_WEBHOOK;
+    const secret = req.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, webhook);
+    } catch (err) {
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    const { type } = event;
+    if (eventRelevant.has(type)) {
+      console.log(event, type);
+    }
+    res.json({ received: true });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
