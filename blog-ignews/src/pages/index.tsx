c2b55c5e125c690a08c65df77902087fc26f1c95
hugovarellaa <hugovarellaa@gmail.com>
2022-02-22 23:06:11 -0300

    Criação e finalização do layout estatico do Header

--- blog-ignews/src/pages/index.tsx
+++ blog-ignews/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>testando</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
+      <main>home</main>
+    </>
+  );
 }
