6ab352632703301527b08d2129d96cf85f97c4a9
hugovarellaa <hugovarellaa@gmail.com>
2022-02-24 14:47:18 -0300

    segundo teste

--- blog-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
+++ blog-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -11,22 +11,24 @@ jest.mock("next/router", () => {
   };
 });
 
-test("active link render correctly", () => {
-  const { getByText } = render(
-    <ActiveLink activeClassName="active" href="/">
-      <a>home</a>
-    </ActiveLink>
-  );
+describe("Active Link components", () => {
+  test("active link render correctly", () => {
+    const { getByText } = render(
+      <ActiveLink activeClassName="active" href="/">
+        <a>home</a>
+      </ActiveLink>
+    );
 
-  expect(getByText("home")).toBeInTheDocument();
-});
+    expect(getByText("home")).toBeInTheDocument();
+  });
 
-test("active link is receiving active class", () => {
-  const { getByText } = render(
-    <ActiveLink activeClassName="active" href="/">
-      <a>home</a>
-    </ActiveLink>
-  );
+  test("active link is receiving active class", () => {
+    const { getByText } = render(
+      <ActiveLink activeClassName="active" href="/">
+        <a>home</a>
+      </ActiveLink>
+    );
 
-  expect(getByText("home")).toHaveClass("active");
+    expect(getByText("home")).toHaveClass("active");
+  });
 });
