c1366de237184f6365660263dc88a67d2975306d
hugovarellaa <hugovarellaa@gmail.com>
2022-02-24 14:46:13 -0300

    segundo teste

--- blog-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
+++ blog-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -20,3 +20,13 @@ test("active link render correctly", () => {
 
   expect(getByText("home")).toBeInTheDocument();
 });
+
+test("active link is receiving active class", () => {
+  const { getByText } = render(
+    <ActiveLink activeClassName="active" href="/">
+      <a>home</a>
+    </ActiveLink>
+  );
+
+  expect(getByText("home")).toHaveClass("active");
+});
