61ed09d1f5f26cffc6b7ce2ed7e046e7c78a72a3
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-11 20:08:47 -0300

    Layout Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,28 @@
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
-      <h1>testando</h1>
+      <Head>Home | Ig.news</Head>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>
+            👏<strong>Hey, welcome</strong>
+          </span>
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications
+            <br /> <span>for $9,90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
