2f027230218376bb62be1a5ebe5d60d463d9e946
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-12 20:50:11 -0300

    Listando posts do Prismi.io na tela

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
@@ -7,10 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo" />
         <nav>
-          <a href="#" className={styles.active}>
-            Home
-          </a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active}  href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
