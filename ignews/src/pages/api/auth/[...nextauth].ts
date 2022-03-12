a1f92351305ecfda5500ec1bb894ece70d0ee7b0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-12 15:03:05 -0300

    Logica de login e cadastramento dele no bando FaunaDB

---
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const {email} = user
+      await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection("users"),
+            {
+              data: { email }
+            }
+          ),
+          q.Get(
+            q.Match(
+              q.Index("user_by_email"),
+              q.Casefold(user.email)
+            )
+          )
+        ),
+      )
+      return true;
+    },
+  },
+});
