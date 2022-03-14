f4bf77c0391b8683e779d257bb002350206daed4
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-14 10:08:10 -0300

    Layout do header

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,7 +1,25 @@
-export function Header (){
+import Link from "next/link";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
   return (
-    <header>
-      <h1>header</h1>
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.svg" alt="logo" />
+
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </nav>
+
+        <SignInButton/>
+      </div>
     </header>
-  )
+  );
 }
