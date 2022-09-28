2d50160320ab07db61f8a96eb8c742eee2916d04
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-28 13:55:36 -0300

    Update chamada aPI

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SuscribeButton'
 import { stripe } from '../services/stripe'
@@ -38,7 +38,7 @@ export default function Home({ product }: Product) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JsSzEH6aihmDxYbnis4Pjbe')
 
   const product = {
@@ -53,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24hours
   }
 }
