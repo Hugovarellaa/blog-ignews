79136528c4f10187a755b84982d0c812bb733b56
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-28 13:26:58 -0300

    Botao de inscrição

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SuscribeButton'
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -20,7 +21,7 @@ export default function Home() {
             <span> for $9,90 month</span>
           </p>
 
-          {/* Subscribe button */}
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="" />
       </main>
