f4bbf0086db00acc0c3bd49ba9ef4f26ebea426e
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-26 12:34:22 -0300

    Layout Header finalizado

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,12 +1,15 @@
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
   const isLog = true
+
   return isLog ? (
     <button className={styles.signInButton}>
       <FaGithub color="var(--green-500)" />
       Hugo Alves Varella
+      <FiX className={styles.closeIcon} />
     </button>
   ) : (
     <button className={styles.signInButton}>
