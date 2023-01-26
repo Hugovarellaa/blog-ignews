f881710747334582e1d0665f5c631e84828629c2
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-26 17:16:29 -0300

    Login next-auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isLog = true
+  const { data: session } = useSession()
 
-  return isLog ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="var(--green-500)" />
-      Hugo Alves Varella
+      {session.user.name}
       <FiX className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="var(--yellow-500)" />
       Sing in with GitHub
     </button>
