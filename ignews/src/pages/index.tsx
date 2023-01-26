75c15e4ffb72b8c02928a23d50f32d0a03698168
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-26 11:41:31 -0300

    estilos globais

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ignews | Home</title>
+      </Head>
       <div>
         <h1>Ola mundo</h1>
       </div>
