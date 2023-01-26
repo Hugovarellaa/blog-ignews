fd340ac190819876f95e6e3cf0b5c714d4d0a0c2
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-26 12:53:51 -0300

    Layout Home finalizado

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="" />
+        <Image src="/images/logo.svg" alt="" width={108} height={30} />
         <nav>
           <a href="" className={styles.active}>
             Home
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './app.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ignews | Home</title>
+        <title>Home | ig.news</title>
       </Head>
-      <div></div>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width={334}
+          height={520}
+        />
+      </main>
     </>
   )
 }
