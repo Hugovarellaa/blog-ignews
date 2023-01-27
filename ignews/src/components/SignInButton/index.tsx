95cee077d36c5a8d354503caba4f0bc67a4d90d5
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-27 18:31:58 -0300

    Salvando usuário no banco, sem duplicidade

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,17 +1,27 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './SignInButton.module.scss'
 
 export function SignInButton() {
-  const isLog = true
-  return isLog ? (
-    <button type="button" className={styles.signInButton}>
+  const { data: session } = useSession()
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="var(--green-500)" />
-      Hugo Alves Varella
+      {session.user.name}
       <FiX className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="var(--yellow-500)" />
       Sing in with GitHub
     </button>
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { fauna } from '@/src/services/faunadb'
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
@@ -15,6 +17,40 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const email = user.email
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
+        )
+
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    },
+
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session({ session, user, token }) {
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
+    },
+  },
 }
 
 export default NextAuth(authOptions)
