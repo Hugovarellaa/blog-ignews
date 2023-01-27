e2bfdde9839ca70f50b08d2009299fb3c2d207e8
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-27 11:40:55 -0300

    Create checkout session stripe

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,9 +1,20 @@
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function SubscribeButton() {
+  const { data: session } = useSession()
+  function handleSubscribe() {
+    if (!session) {
+      return signIn('github')
+    }
+    // Criar checkout session stripe
+  }
+
   return (
     <>
-      <button className={styles.subscribeButton}>Subscribe now</button>
+      <button className={styles.subscribeButton} onClick={handleSubscribe}>
+        Subscribe now
+      </button>
     </>
   )
 }
