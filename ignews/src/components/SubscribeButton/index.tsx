77eb49ffa5285af9ead503e880491daaa9f13022
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-27 18:59:47 -0300

    Function Checkout session stripe

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,9 +1,34 @@
+import { api } from '@/src/services/axios'
+import { getStripeJs } from '@/src/services/stripe-js'
+import { signIn, useSession } from 'next-auth/react'
 import styles from './SubscribeButton.module.scss'
 
 export function SubscribeButton() {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('subscribe')
+      const { sessionId } = response.data
+
+      const stripe = getStripeJs()
+      await (await stripe).redirectToCheckout({ sessionId })
+    } catch (error) {
+      console.log('Erro:', error)
+    }
+  }
   return (
     <>
-      <button type="button" className={styles.subscribeButtonContainer}>
+      <button
+        type="button"
+        className={styles.subscribeButtonContainer}
+        onClick={handleSubscribe}
+      >
         Subscribe now
       </button>
     </>
