82ea828f8e9e84cfb4fd379349f59475ca46a3c5
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-27 17:08:31 -0300

    Header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import styles from './Header.module.scss'
+
+export function Header() {
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.headeContent}>
+          <Image
+            src="./images/logo.svg"
+            alt="logo Ignews"
+            width={108}
+            height={30}
+          />
+
+          <nav>
+            <a href="" className={styles.active}>
+              Home
+            </a>
+            <a href="">Post</a>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Ola mundo</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+
+      <div>
+        <h1>Ola mundo</h1>
+      </div>
+    </>
   )
 }
