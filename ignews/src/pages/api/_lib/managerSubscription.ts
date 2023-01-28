5d28a28dd5c0dd22d5fb59767f89e9022ec5547f
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-28 15:42:16 -0300

    Trantando e salvando eventos de Delete e Update do stripe e salvando no Banco de dados FaunaDB

--- ignews/src/pages/api/_lib/managerSubscription.ts
+++ ignews/src/pages/api/_lib/managerSubscription.ts
@@ -8,6 +8,7 @@ import { query as q } from 'faunadb'
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) {
   const userRef = await fauna.query(
     q.Select('ref', q.Get(q.Match(q.Index('user_by_customer_id'), customerId))),
@@ -22,9 +23,23 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   }
 
-  await fauna.query(
-    q.Create(q.Collection('subscriptions'), {
-      data: subscriptionData,
-    }),
-  )
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection('subscriptions'), {
+        data: subscriptionData,
+      }),
+    )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId)),
+        ),
+        {
+          data: subscriptionData,
+        },
+      ),
+    )
+  }
 }
