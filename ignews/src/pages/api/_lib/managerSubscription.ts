2cfba75abbaf8db08a1b30f3725838e7634efe16
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-28 15:23:53 -0300

    Salvando informações de compras do stripe no banco de dados FaunaDB

--- ignews/src/pages/api/_lib/managerSubscription.ts
+++ ignews/src/pages/api/_lib/managerSubscription.ts
@@ -1,9 +1,30 @@
 // Buscar o usuário no banco do fauna pelo Id do Stripe (stripe_customer_id)
 // Salvar os dados da subscription do usuário no banco de dados -> FaunaDB (na collection subscriptions)
 
+import { fauna } from '@/src/services/faunadb'
+import { stripe } from '@/src/services/stripe'
+import { query as q } from 'faunadb'
+
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
 ) {
-  console.log(subscriptionId, customerId)
+  const userRef = await fauna.query(
+    q.Select('ref', q.Get(q.Match(q.Index('user_by_customer_id'), customerId))),
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  await fauna.query(
+    q.Create(q.Collection('subscriptions'), {
+      data: subscriptionData,
+    }),
+  )
 }
