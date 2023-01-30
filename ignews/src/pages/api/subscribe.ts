230e5f022943c71812a828d4c541a7f53d7c09d7
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-30 17:57:35 -0300

    Create Checkout Session Stripe

---
+++ ignews/src/pages/api/subscribe.ts
@@ -0,0 +1,29 @@
+import { stripe } from '@/src/services/stripe'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const session = await getSession({ req: request })
+
+    const stripeCustomerId = await stripe.customers.create({
+      email: session.user.email,
+    })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomerId.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      line_items: [{ price: 'price_1JsSzEH6aihmDxYbnis4Pjbe', quantity: 1 }],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return response.status(201).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    response.setHeader('Allow', 'POST')
+    return response.status(405).send('Method not allowed')
+  }
+}
