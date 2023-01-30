1dae89cebadb7869bfb331f7548b7dcfc79ee83a
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-30 17:57:58 -0300

    Start checkout session

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,9 +1,28 @@
+import { api } from '@/src/services/axios'
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function SubscribeButton() {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      await signIn('github')
+    }
+    try {
+      await api.post('subscribe')
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <>
-      <button type="button" className={styles.subscribeButton}>
+      <button
+        type="button"
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+      >
         Subscribe now
       </button>
     </>
