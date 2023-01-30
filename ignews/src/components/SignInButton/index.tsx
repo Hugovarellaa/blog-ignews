e4e3027540411f1eca5849d1a21d738a82c90c07
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-30 13:41:57 -0300

    Button sign in with github

---
+++ ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+  const isLog = true
+  return isLog ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--green)" />
+      Hugo Alves Varella
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--yellow)" />
+      Sing in with GitHub
+    </button>
+  )
+}
