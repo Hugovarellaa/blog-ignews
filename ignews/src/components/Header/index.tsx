1c55afb5409328139eff047cdcb9ff221ed0e813
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-30 13:41:36 -0300

    Initial component header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import { SignInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src="/images/logo.svg"
+          width={108}
+          height={30}
+          alt="Logo Ignews"
+        />
+        <nav>
+          <a href="#" className={styles.active}>
+            Home
+          </a>
+          <a href="#">Post</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  )
+}
