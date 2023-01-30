cb23f29fc2a12294628333c4c1673e887154ef47
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-30 16:31:27 -0300

    Layout Home page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,33 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './app.module.scss'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Ola mundo</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.appContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+        <Image
+          src="images/avatar.svg"
+          alt="Girl coding"
+          width={334}
+          height={520}
+        />
+      </main>
+    </>
   )
 }
